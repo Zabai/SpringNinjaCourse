c43b36308e19ede7ddbbac942dee07c5e5a59293
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-21 23:15:59 +0100

    Redirections

--- src/main/java/com/zarmas/ninjabackend/controller/Example3Controller.java
+++ src/main/java/com/zarmas/ninjabackend/controller/Example3Controller.java
@@ -8,12 +8,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
     private static final String FORM_VIEW = "form";
     private static final String FORM_RESULT = "result";
+
+    // First way to redirect
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:/example3/new";
+    }
+
+    // Second way to redirect
+    @GetMapping("/")
+    public RedirectView redirectView() {
+        return new RedirectView("/example3/new");
+    }
 
     @GetMapping("/new")
     public String showForm(Model model) {
