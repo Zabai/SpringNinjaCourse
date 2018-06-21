17690ac55c00a0fcbe6709c0c4278f170b2ff974
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-22 00:05:24 +0100

    Request Interceptor
    Executes code before controller's methods and before rendering templates

---
+++ src/main/java/com/zarmas/ninjabackend/component/RequestTimeInterceptor.java
@@ -0,0 +1,30 @@
+package com.zarmas.ninjabackend.component;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("requestTimeInterceptor")
+public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
+    private final Log log = LogFactory.getLog(RequestTimeInterceptor.class);
+
+    // Executes before controller's method
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return super.preHandle(request, response, handler);
+    }
+
+    // Executes before render templates
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (long) request.getAttribute("startTime");
+        long time = System.currentTimeMillis() - startTime;
+        log.info("-- Request URL: '" + request.getRequestURL().toString() + "' -- TOTAL TIME: '" + time + "'ms");
+        super.afterCompletion(request, response, handler, ex);
+    }
+}
---
+++ src/main/java/com/zarmas/ninjabackend/configuration/WebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package com.zarmas.ninjabackend.configuration;
+
+import com.zarmas.ninjabackend.component.RequestTimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Autowired
+    @Qualifier("requestTimeInterceptor")
+    private RequestTimeInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+}
