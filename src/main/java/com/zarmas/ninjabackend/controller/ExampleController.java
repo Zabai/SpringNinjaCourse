d4dd8d0f428d4e365fc5cfd9961c5e692d4e4843
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-21 16:22:06 +0100

    Passing list data to views

--- src/main/java/com/zarmas/ninjabackend/controller/ExampleController.java
+++ src/main/java/com/zarmas/ninjabackend/controller/ExampleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
@@ -16,7 +19,7 @@ public class ExampleController {
     // First Way
     @GetMapping("/exampleString")
     public String exampleString(Model model) {
-        model.addAttribute("person", new Person("General Kenobi", 34));
+        model.addAttribute("people", getPersonList());
         return EXAMPLE_VIEW;
     }
 
@@ -24,8 +27,19 @@ public class ExampleController {
     @RequestMapping(value = "/exampleMAV", method = RequestMethod.GET)
     public ModelAndView exampleMAV() {
         ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-        mav.addObject("person", new Person("Yoda", 99));
+        mav.addObject("people", getPersonList());
 
         return mav;
+    }
+
+    private List<Person> getPersonList() {
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(new Person("General Kenobi", 34));
+        personList.add(new Person("Yoda", 99));
+        personList.add(new Person("Peter Parker", 24));
+        personList.add(new Person("Deadpool", 32));
+
+        return personList;
     }
 }
