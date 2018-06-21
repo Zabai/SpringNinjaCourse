1a87eafa5f220725adf45e56eaf94ea1ad900660
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-21 14:58:57 +0100

    Application properties to yml

---
+++ src/main/java/com/zarmas/ninjabackend/NinjaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.zarmas.ninjabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaBackendApplication.class, args);
+    }
+}
