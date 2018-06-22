313dfc17c1983df5bc65b4877800b3beb083d328
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-22 11:08:17 +0100

    Form Validation

--- src/main/java/com/zarmas/ninjabackend/controller/Example3Controller.java
+++ src/main/java/com/zarmas/ninjabackend/controller/Example3Controller.java
@@ -5,11 +5,14 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/example3")
@@ -32,11 +35,16 @@ public class Example3Controller {
     }
 
     @PostMapping("/addperson")
-    public ModelAndView addPerson(@ModelAttribute("person") Person person) {
-        LOGGER.info("METHOD: 'addPerson' -- PARAMS: '" + person + "'");
-        ModelAndView mav = new ModelAndView(FORM_RESULT);
-        mav.addObject("person", person);
-        LOGGER.info("TEMPLATE: '" + FORM_RESULT + ".html' --- DATA: '" + person + "'");
+    public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult) {
+        ModelAndView mav = new ModelAndView();
+
+        if(bindingResult.hasErrors()) {
+            mav.setViewName(FORM_VIEW);
+        } else {
+            mav.setViewName(FORM_RESULT);
+            mav.addObject("person", person);
+        }
+
         return mav;
     }
 }
