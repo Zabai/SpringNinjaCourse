a5c4ac5ab6eed4e9c1106c346d12981a4812210d
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-22 14:41:33 +0100

    'Contact' Entity and Model

---
+++ src/main/java/com/zarmas/ninjabackend/entity/Contact.java
@@ -0,0 +1,70 @@
+package com.zarmas.ninjabackend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "telephone")
+    private String telephone;
+    @Column(name = "city")
+    private String city;
+
+    public Contact() {}
+
+    public Contact(int id, String firstName, String lastName, String telephone, String city) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
