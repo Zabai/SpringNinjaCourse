290821f198fb3ee3bd2d26a757d6fdf4bb7b9f5a
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-06-25 13:47:57 +0100

    'User' and 'UserRole' entities

---
+++ src/main/java/com/zarmas/ninjabackend/entity/UserRole.java
@@ -0,0 +1,60 @@
+package com.zarmas.ninjabackend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_roles",
+        uniqueConstraints = @UniqueConstraint(columnNames = { "role", "username"}))
+public class UserRole {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username", nullable = false)
+    private User user;
+
+    @Column(name = "role", nullable = false, length = 45)
+    private String role;
+
+    public UserRole() {}
+
+    public UserRole(User user, String role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", user=" + user +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
